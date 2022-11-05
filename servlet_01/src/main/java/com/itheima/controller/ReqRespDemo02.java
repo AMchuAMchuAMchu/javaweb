4328635a78874779c6544e18cb43ddc6a0bcd974
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-05 09:24:45 +0800

    shino_hecate

---
+++ servlet_01/src/main/java/com/itheima/controller/ReqRespDemo02.java
@@ -0,0 +1,81 @@
+package com.itheima.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet("reqrespdemo02")
+public class ReqRespDemo02 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("get...");
+
+        String method = request.getMethod();
+
+        String contextPath = request.getContextPath();
+
+        String requestURI = request.getRequestURI();
+
+        StringBuffer requestURL = request.getRequestURL();
+
+        String queryString = request.getQueryString();
+
+        String name = request.getParameter("name");
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+
+        String[] names = request.getParameterValues("names");
+
+        System.out.println("method::"+method);
+        System.out.println("contextPath::"+contextPath);
+        System.out.println("requestURI::"+requestURI);
+        System.out.println("requestURL::"+requestURL);
+        System.out.println("queryString::"+queryString);
+        System.out.println("name::"+name);
+        System.out.println("parameterNames::");
+        System.out.println(parameterNames);
+        System.out.println("names::");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(i+1+">>"+names[i]);
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("post...");
+
+        String method = request.getMethod();
+
+        String contextPath = request.getContextPath();
+
+        String requestURI = request.getRequestURI();
+
+        StringBuffer requestURL = request.getRequestURL();
+
+        String queryString = request.getQueryString();
+
+        String name = request.getParameter("name");
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+
+        String[] names = request.getParameterValues("names");
+
+        System.out.println("method::"+method);
+        System.out.println("contextPath::"+contextPath);
+        System.out.println("requestURI::"+requestURI);
+        System.out.println("requestURL::"+requestURL);
+        System.out.println("queryString::"+queryString);
+        System.out.println("name::"+name);
+        System.out.println("parameterNames::");
+        System.out.println(parameterNames);
+        System.out.println("names::");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(i+1+">>"+names[i]);
+        }
+
+    }
+}
