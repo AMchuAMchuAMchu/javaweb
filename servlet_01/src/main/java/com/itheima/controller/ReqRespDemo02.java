7f1693994892647b18754bc77db40e3bad15a51d
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-05 09:26:49 +0800

    shino_hecate

--- servlet_01/src/main/java/com/itheima/controller/ReqRespDemo02.java
+++ servlet_01/src/main/java/com/itheima/controller/ReqRespDemo02.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.Enumeration;
 
-@WebServlet("reqrespdemo02")
+@WebServlet("/reqrespdemo02")
 public class ReqRespDemo02 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
