a1769efb9583ff30dc40e5da433c1835499c638d
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-07 07:50:57 +0800

    AM

---
+++ reqresp_01/src/main/java/com/itheima/controller/Redirect01.java
@@ -0,0 +1,32 @@
+package com.itheima.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> javaweb
+ * BelongsPackage ==> com.itheima.controller
+ * Version ==> 1.0
+ * CreateTime ==> 2022-11-07 07:46:03
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@WebServlet("/redirect01")
+public class Redirect01 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet...");
+        System.out.println("redirect01");
+        req.getRequestDispatcher("/redirect02").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost...");
+    }
+}
