60a59a4c55e5eb78254961dfeffcc99096bdb729
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-07 07:54:03 +0800

    AM

--- reqresp_01/src/main/java/com/itheima/controller/Redirect01.java
+++ reqresp_01/src/main/java/com/itheima/controller/Redirect01.java
@@ -22,8 +22,9 @@ public class Redirect01 extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doGet...");
         System.out.println("redirect01");
-        req.setAttribute("name","刀剑神域 10 anniversary");
-        req.getRequestDispatcher("/redirect02").forward(req,resp);
+//        req.setAttribute("name","刀剑神域 10 anniversary");
+//        req.getRequestDispatcher("/redirect02").forward(req,resp);
+        resp.getWriter().write("欢迎来到刀剑的殿堂....");
     }
 
     @Override
