88e2ab97fc5d1a63f4f91cc2c22f6e23817b1f50
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-07 08:17:44 +0800

    AM

--- reqresp_01/src/main/java/com/itheima/controller/Redirect01.java
+++ reqresp_01/src/main/java/com/itheima/controller/Redirect01.java
@@ -30,9 +30,9 @@ public class Redirect01 extends HttpServlet {
         resp.getWriter().write("<h1>欢迎来到刀剑的殿堂....</h1>");
         resp.setStatus(302);
         String contextPath = req.getContextPath();
-//        resp.setHeader("Location",contextPath+"/redirect02");
+        resp.setHeader("Location",contextPath+"/redirect02");
 //        resp.setHeader("Location","/baidu.com");
-        resp.sendRedirect("http://www.baidu.com");
+//        resp.sendRedirect("http://www.baidu.com");
     }
 
     @Override
