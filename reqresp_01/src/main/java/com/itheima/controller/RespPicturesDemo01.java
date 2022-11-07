05069b68e5cd618066138da9825683a4053a3d37
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-07 08:28:53 +0800

    AM

--- reqresp_01/src/main/java/com/itheima/controller/RespPicturesDemo01.java
+++ reqresp_01/src/main/java/com/itheima/controller/RespPicturesDemo01.java
@@ -24,7 +24,7 @@ public class RespPicturesDemo01 extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doGet...");
         ServletOutputStream os = resp.getOutputStream();
-        FileInputStream fis = new FileInputStream("d:\\ke.jpg");
+        FileInputStream fis = new FileInputStream("d://ke.jpg");
         byte[] bytes = new byte[1024];
         int len = 0;
         while ((len = fis.read(bytes))!=-1){
