b1ba35a6644f5fc6e18967aa091f0a311adec18f
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-07 08:25:39 +0800

    AM

---
+++ reqresp_01/src/main/java/com/itheima/controller/RespPicturesDemo01.java
@@ -0,0 +1,39 @@
+package com.itheima.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> javaweb
+ * BelongsPackage ==> com.itheima.controller
+ * Version ==> 1.0
+ * CreateTime ==> 2022-11-07 08:22:04
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@WebServlet("/resppic01")
+public class RespPicturesDemo01 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet...");
+        resp.setContentType("text/html;charset=utf-8");
+        ServletOutputStream os = resp.getOutputStream();
+        FileInputStream fis = new FileInputStream("d://ke.jpg");
+        byte[] bytes = new byte[1024];
+        int len = 0;
+        while ((len = fis.read(bytes))!=-1){
+            os.write(bytes,0,-1);
+        }
+
+        fis.close();
+        System.out.println("图片已经成功的写出的说...");
+
+    }
+}
