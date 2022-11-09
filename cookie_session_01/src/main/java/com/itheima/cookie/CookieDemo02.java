89f552d434528e5885a0346cee07e4886a07bc57
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-09 08:02:09 +0800

    AM

---
+++ cookie_session_01/src/main/java/com/itheima/cookie/CookieDemo02.java
@@ -0,0 +1,36 @@
+package com.itheima.cookie;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> javaweb
+ * BelongsPackage ==> com.itheima.cookie
+ * Version ==> 1.0
+ * CreateTime ==> 2022-11-09 07:56:46
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@WebServlet("/cookie02")
+public class CookieDemo02 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("doGet...");
+
+        Cookie[] cookies = req.getCookies();
+
+        for (Cookie cookie : cookies) {
+            if ("name".equals(cookie.getName())){
+                System.out.println(cookie.getName()+"::"+cookie.getValue());
+            }
+        }
+
+    }
+}
