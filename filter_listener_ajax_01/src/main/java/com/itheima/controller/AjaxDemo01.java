909c3633e335d006cde6f9902ec6fb2beb2ed79b
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-10 17:14:39 +0800

    AM

---
+++ filter_listener_ajax_01/src/main/java/com/itheima/controller/AjaxDemo01.java
@@ -0,0 +1,35 @@
+package com.itheima.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> javaweb
+ * BelongsPackage ==> com.itheima.controller
+ * Version ==> 1.0
+ * CreateTime ==> 2022-11-10 17:09:36
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@WebServlet("/ajaxdemo01")
+public class AjaxDemo01 extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("doGet...");
+
+        HttpSession session = req.getSession();
+
+        session.setAttribute("name01","在地下城寻求邂逅是否搞错了什么...");
+
+
+
+    }
+}
