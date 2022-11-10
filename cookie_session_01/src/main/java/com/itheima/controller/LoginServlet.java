e728e83e58d596f0215794fe116f4b1d84cb78aa
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-10 10:48:23 +0800

    AM

--- cookie_session_01/src/main/java/com/itheima/controller/LoginServlet.java
+++ cookie_session_01/src/main/java/com/itheima/controller/LoginServlet.java
@@ -69,6 +69,40 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doPost...");
+        System.out.println("doGet...");
+
+        String username = req.getParameter("username");
+
+        String password = req.getParameter("password");
+
+        //2. 调用MyBatis完成查询
+        //2.1 获取SqlSessionFactory对象
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        //2.2 获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //2.3 获取Mapper
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        //2.4 调用方法
+        User user = userMapper.select(username, password);
+        //2.5 释放资源
+        sqlSession.close();
+
+        //获取字符输出流，并设置content type
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+        //3. 判断user释放为null
+        if(user != null){
+            // 登陆成功
+            writer.write("登陆成功");
+        }else {
+            // 登陆失败
+            writer.write("登陆失败");
+        }
+
+
+
 
     }
 }
